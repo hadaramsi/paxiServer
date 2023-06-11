d4dc419fa2ac08a32ced85e69125c05d0d736aaa
hadar.amsalem <73754433+hadaramsi@users.noreply.github.com>
2023-06-12 00:01:30 +0300

    buildGraph function

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import flask
+from datetime import date, datetime
 import firebase_admin
 # from firebase_admin import db
 from firebase_admin import firestore
@@ -10,8 +11,7 @@ app = flask.Flask(__name__)
 def extract_name():
     res = "Paxi server is up"
     cred_obj = firebase_admin.credentials.Certificate(r"C:\Users\Hadar\Desktop\paxi\paxi-2926b-firebase-adminsdk-yqehw-fe79636c2c.json")
-    firebase_admin.initialize_app(cred_obj, {
-	'databaseURL':"https://console.firebase.google.com/u/0/project/paxi-2926b/database"})
+    firebase_admin.initialize_app(cred_obj, {'databaseURL':"https://console.firebase.google.com/u/0/project/paxi-2926b/database"})
     db = firestore.client()
     doc_ref = db.collection('routes').document('NJXWjum9WchLU3N58zad')
     doc = doc_ref.get()
@@ -28,5 +28,59 @@ def extract_name():
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
 
+
+def buildGraph():
+    cred_obj = firebase_admin.credentials.Certificate(
+        r"C:\Users\Hadar\Desktop\paxi\paxi-2926b-firebase-adminsdk-yqehw-fe79636c2c.json")
+    firebase_admin.initialize_app(cred_obj, {
+        'databaseURL': "https://console.firebase.google.com/u/0/project/paxi-2926b/database"})
+    db = firestore.client()
+    docsPacks = db.collection('packages').get()
+    for doc in docsPacks:
+        graph = {}
+        s = doc.get('source')
+        d = doc.get('destination')
+        dDate = doc.get('date')
+        cost = doc.get('cost')
+        w = doc.get('weight')
+        v = doc.get('volume')
+        docsRoutes = db.collection('routes').get()
+        for i in docsRoutes:
+            if w <= i.get('weight') and v <= i.get('volume'):
+                if datetime.strptime(i.get('date'), '%m/%d/%y %H:%M:%S') <= datetime.strptime(dDate, '%m/%d/%y %H:%M:%S'):
+                    if cost >= i.get('cost'):
+                        graph.update({i.get('futureRouteID'): i.get('cost')})
+    PackMatch(graph, s, d, dDate, cost)
+
+
+def PackMatch(Graph, source, destination, dDate, cost):
+    currentDate = date.today()
+#     create vertex set Q
+#
+# # Initialization
+#
+#     for each vertex v in Graph:
+#         dist[v] = INFINITY
+#         prev[v] = UNDEFINED
+#         add v to Q
+#
+#     dist[source] = 0 # Distance from source to source
+#
+#     while Q is not empty:
+#         u = vertex in Q
+#         with min dist[u]
+#         remove u from Q
+#
+#         for each neighbor v of u in range dates:
+#             alt = dist[u] + length(u, v)
+#             if alt < dist[v]:
+#                 dist[v] = alt
+#                 prev[v] = u
+#                 cDate = Weight.date
+#
+#     if dist[destination] > cost:
+#         return null
+#
+#     return dist[], prev[]
 
 
