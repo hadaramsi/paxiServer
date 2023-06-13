988bc9fe324217b3b778b96fde5df6bacdd6aad1
hadar.amsalem <73754433+hadaramsi@users.noreply.github.com>
2023-06-13 18:34:31 +0300

    dijkstra algorithm function

--- main.py
+++ main.py
@@ -41,32 +41,63 @@ def buildGraph():
                                                                         'date': r.get('date'),
                                                                         'cost': r.get('cost')}
                         # graph.update({r.get('source'): r.get('destination')} {r.get('futureRouteID'): r.get('cost')}})
-        print(graph)
-        nodes = PackMatch(graph, s, d, cost)
+        # print(graph)
+        nodes= dijkstra(graph, s)
+        # nodes = PackMatch(graph, s, d, cost)
+        if nodes[d][0]!= float('inf'):
+            pass
+        print( " this is what we wont to seeeeee")
         print(nodes)
 
 
-def PackMatch(graph, source, destination, cost):
-  nodes = {}
-  c = 0
-  for node in graph:
-      nodes[node] = Node()
-  nodes[source].d = 0
-  queue = [(0, source)] #priority queue
-  while queue:
-      d, node = heapq.heappop(queue)
-      if nodes[node].finished:
-          continue
-      nodes[node].finished = True
-      for neighbor in graph[node]:
-          if nodes[neighbor].finished:
-              continue
-          new_d = d+graph[node][neighbor]
-          if new_d < nodes[neighbor].d:
-              nodes[neighbor].d = new_d
-              nodes[neighbor].parent = node
-              heapq.heappush(queue, (new_d, neighbor))
-  return nodes
+def dijkstra(graph, start):
+    distances = {}
+    for vertex in graph:
+        distances[vertex] = [float('inf'), None]
+    distances[start][0] = 0
+
+    priority_queue = [(0, start)]
+    heapq.heapify(priority_queue)
+    while priority_queue:
+        current_distance, current_vertex = heapq.heappop(priority_queue)
+
+        # Ignore outdated entries in the priority queue
+        # print("1111111" + str(distances))
+        if current_distance > distances[current_vertex][0]:
+            continue
+
+        for neighbor, weight in graph[current_vertex].items():
+            distance = current_distance + weight['cost']
+            # print(neighbor)
+            # print("weight['cost']-----" + str(weight['cost']))
+            if distance < distances[neighbor][0]:
+                distances[neighbor][0] = distance
+                distances[neighbor][1]=current_vertex
+                heapq.heappush(priority_queue, (distance, neighbor))
+    return distances
+
+
+# def PackMatch(graph, source, destination, cost):
+#   nodes = {}
+#   c = 0
+#   for node in graph:
+#       nodes[node] = Node()
+#   nodes[source].d = 0
+#   queue = [(0, source)] #priority queue
+#   while queue:
+#       d, node = heapq.heappop(queue)
+#       if nodes[node].finished:
+#           continue
+#       nodes[node].finished = True
+#       for neighbor in graph[node]:
+#           if nodes[neighbor].finished:
+#               continue
+#           new_d = d+graph[node][neighbor]
+#           if new_d < nodes[neighbor].d:
+#               nodes[neighbor].d = new_d
+#               nodes[neighbor].parent = node
+#               heapq.heappush(queue, (new_d, neighbor))
+#   return nodes
 
 
 # def PackMatch(Graph, source, destination, dDate, cost):
